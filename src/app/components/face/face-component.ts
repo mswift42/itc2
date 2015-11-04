7e757a9b48ee07dbea16d4053bdc823eac62936c
mswift42 <martin.haesler@gmail.com>
2015-11-04 14:10:27 +0000

    Add face-component

---
+++ src/app/components/face/face-component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/angular2';
+
+@Component({
+  selector: 'theme-face',
+  templateUrl: 'app/components/face-component/face-component.html'
+})
+
+  export class FaceComponent {}
---
+++ src/app/components/input/input-component.ts
@@ -0,0 +1,8 @@
+import {Component} from 'angular2/angular2';
+
+@Component({
+    selector: 'input',
+    templateUrl: 'app/components/input/input-component.html'
+})
+
+export class InputComponent {}
