8144ccc6fb0cbba0cfa71974b69eb5ae619a11e3
mswift42 <martin.haesler@gmail.com>
2015-11-18 18:57:03 +0000

    add ThemeService

---
+++ src/app/services/theme-service.ts
@@ -0,0 +1,11 @@
+import {Theme} from '../Theme';
+import {Face} from '../Face';
+import {Injectable} from 'angular2/angular2';
+
+@Injectable()
+export class ThemeService {
+	theme: Theme;
+	
+	constructor() {}
+
+}
